18f12c75d575ece9cbf722d37f6c6c8c1ffc7088
Jesse Schalken <me@jesseschalken.com>
2015-10-08 11:34:01 +1100

    Adjust fish terminal colors again

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -32,9 +32,9 @@ function fish_prompt
     echo -n ' '
     switch $USER
         case root
-            set_color -o -b 900 f33
+            set_color -o -b 800 f66
         case '*'
-            set_color -o -b 00f 99f
+            set_color -o -b 00f 88f
     end
     echo -n 'λ'
     set_color -b normal normal
